2bf21205989366c0214f56957d0698f9b66f2745
Georges Stefan <stefangeorges4799@gmail.com>
2020-08-10 15:18:06 -0300

    Added images from network and from assets.

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,14 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My App'),
+        title: Text('Dan Jackson'),
         centerTitle: true,
         backgroundColor: Colors.amber[700],
       ),
       body: Center(
-        child: Text(
-          'Dimi Dimi a',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'Lemonada',
-          ),
+        child: Image(
+          //image: NetworkImage('https://images.unsplash.com/photo-1588098928842-715f1673f6b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+          image: AssetImage('assets/danjackson.png'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
